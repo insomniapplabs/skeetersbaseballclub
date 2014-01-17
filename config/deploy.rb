5f379ec217a9ed52d54da1148748c4fdf1010049
Aaron Johnson <aaronjohnsonis@gmail.com>
2014-01-16 23:14:54 -0600

    update db.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,6 +71,14 @@ after "deploy", "deploy:cleanup" #keep only the last 5 releases
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
+	namespace :assets do
+    task :precompile, :roles => assets_role, :except => { :no_release => true } do
+      run <<-CMD.compact
+        cd -- #{latest_release.shellescape} &&
+        #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile
+      CMD
+    end
+  end
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
